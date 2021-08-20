309ad5f15fcf6b483a0daa84197c73d5069b0966
zinkoadh <pavel.karabinovich@gmail.com>
2021-08-20 23:10:39 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        this.result = Math.ceil((this.max+this.min)/2)
+        return this.result
     }
 
     lower() {
-
+        this.max = this.result
     }
 
     greater() {
-
+        this.min = this.result
     }
 }
 
